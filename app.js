0e48d67b6a0d393ceeacc05d5516c1fcb6066aab
omardgz97 <omardgz97@gmail.com>
2024-08-14 06:58:31 -0600

    ultimos detalles en el DOM para la interactividad de los botones con el mouse, agregué el favicon y la imagen dinámica para el textareaSalida

--- app.js
+++ app.js
@@ -6,25 +6,51 @@ const imagenDesencriptar = document.querySelector('.imagen__Desencriptar');
 const textAreaSalida =document.getElementById('textarea__Salida');
 const botonCopiar = document.getElementsByClassName('boton__Copiar')[0];
 const imagenesFooter = document.getElementsByClassName('imagenesFooter');
+const todosLosBotones = document.getElementsByTagName('button');
 
-//------------------------Funciones para Encriptar y desencriptar---------------------------//
+//----------------------Funciones para botones Encriptar, Desencriptar y Copiar--------------------------//
 
-botonEncriptar.onclick = function(){
+botonEncriptar.addEventListener('click', function(){
+    verificacionDeTextoValido()
     let textoEncriptado = encriptar(textAreaEntrada.value);
-    console.log(textoEncriptado);
     textAreaSalida.value = textoEncriptado;
     ocultarBotonCopiar();
     ocultarImagen();
-};
+});
 
-botonDesencriptar.onclick = function(){
+botonDesencriptar.addEventListener('click', function(){
+    verificacionDeTextoValido()
     let textoDesencriptado = desencriptar(textAreaEntrada.value);
     textAreaSalida.value = textoDesencriptado;
     ocultarBotonCopiar();
     ocultarImagen();
+});
+
+botonCopiar.addEventListener('click', function(){
+    navigator.clipboard.writeText(textAreaSalida.value);
+});
+
+Array.from(todosLosBotones).forEach(boton => {
+    const colorOriginal = boton.style.backgroundColor;
+    boton.addEventListener('mousedown', function() {
+        boton.style.backgroundColor = '#021526';
+    });
+    boton.addEventListener('mouseup', function(){
+        boton.style.backgroundColor = colorOriginal;
+    });
+});
+
+function verificacionDeTextoValido(){
+    let expresionRegular = /[A-ZáéíóúÁÉÍÓÚ]/;
+    if (expresionRegular.test(textAreaEntrada.value)){
+        alert('Tu texto contiene mayúsculas o acentos.');
+        throw new Error;
+    } else {
+        return;
+    };
 };
 
-//------------------Funciones para borrar texto cuando una de las dos cajas de texto esta vacia--------//
+//------------------Funciones para borrar texto cuando una de las dos cajas de texto esta vacia----------//
 
 textAreaEntrada.addEventListener('input', function(){
     if (textAreaEntrada.value === ""){
@@ -42,13 +68,15 @@ textAreaSalida.addEventListener('input', function(){
     ocultarImagen();
 });
 
-//---------------------funciones para eliminar imagen y boton de copiar--------------------------------//
+//------------------------funciones para eliminar imagen y boton de copiar-------------------------------//
 
 function ocultarBotonCopiar(){
     if (textAreaSalida.value === ''){
         botonCopiar.style.display = 'none';
+        textAreaSalida.style.height = '100%';
     } else {
         botonCopiar.style.display = 'block';
+        textAreaSalida.style.height = '90%';
     }
 };
 
@@ -60,7 +88,7 @@ function ocultarImagen(){
     }
 };
 
-// FUNCIONES PARA ENCRIPTAR Y DESENCRIPTAR EL CODIGO //
+//------------------------FUNCIONES PARA ENCRIPTAR Y DESENCRIPTAR EL CODIGO-------------------------------//
 
 function encriptar(texto){
     let array = Array.from(texto);
